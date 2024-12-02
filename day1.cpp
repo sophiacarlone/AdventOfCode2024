d82d53509925dc3e6ef9c35ab0148140df48b148
sophiacarlone <sophiamcar@gmail.com>
2024-12-01 23:22:58 -0500

    day1 complete

--- day1.cpp
+++ day1.cpp
@@ -13,7 +13,8 @@ int main()
     file.open("file.txt");
     int input;
     int count = 0;
-    int sum = 0;
+    int sum1 = 0;
+    int sum2 = 0;
     
     while(file >> input){
 	    if(count % 2 == 0)
@@ -39,16 +40,25 @@ int main()
     left.sort();
     right.sort();
 
+    //part 1
     auto lit = left.begin();
     auto rit = right.begin();
     for(int i = 0; i < left.size(); i++){
-	    sum += abs(*lit - *rit);
+	    sum1 += abs(*lit - *rit);
 	    ++lit;
 	    ++rit;
     }
-
-    cout << sum << endl;    
+    cout << sum1 << endl;    
     
+    //part 2
+    for(lit = left.begin(); lit != left.end(); ++lit){
+    	for(rit = right.begin(); rit != right.end(); ++rit){
+	    if(*lit == *rit)
+		    sum2 += *lit;
+	}
+    }
+    cout << sum2 << endl;    
+
     file.close();
     return 0;
 }
