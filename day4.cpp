8854edc11e7a966b8ca79e39cbdeb3f4ea6b3c50
sophiacarlone <sophiamcar@gmail.com>
2025-01-03 17:51:49 -0500

    dinner

--- day4.cpp
+++ day4.cpp
@@ -1,3 +1,5 @@
+//Idea, if we really want speed, doing this with threads would do
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -5,18 +7,23 @@
 
 using namespace std;
 
-#define COMPLETION 3 //end of the needed word
+#define COMPLETION 4 //end of the needed word
 
+//Globals
 int rowMax;
 int colMax;
+int sum;
+vector<vector<int>> puzzle; //would like to avoid the heap if possible, but idk
+const map<char, int> m = {{'X', 1}, {'M', 2}, {'A', 3}, {'S', 4}};
+    
+//Functions
+void neighbors(int letter, int row, int col);
 
 int main()
 {
     ifstream file;
     file.open("input.txt");
     vector<int> v; //one line from input (one row)
-    vector<vector<int>> puzzle;
-    const map<char, int> m = {{'X', 0}, {'M', 1}, {'A', 2}, {'S', 3}};
     
     string line;
     int lineNum = 0;
@@ -30,14 +37,55 @@ int main()
         v.clear();
         lineNum++;
     }
-    for(int i = 0; i < puzzle.size(); i++){
-        for (int j = 0; j < puzzle[i].size(); j++)
-            cout << puzzle[i][j];
-        cout << endl;
-    }
+    // for(int i = 0; i < puzzle.size(); i++){
+    //     for (int j = 0; j < puzzle[i].size(); j++)
+    //         cout << puzzle[i][j];
+    //     cout << endl;
+    // }
     rowMax = puzzle.size();
     colMax = puzzle[0].size();
 
+    //Part 1
+    for(int i = 0; i < rowMax; i++){
+        for (int j = 0; j < colMax; j++){
+            if(puzzle[i][j] == 0) neighbors(0, i, j);
+            cout << "found x at " << i << ", " << j << " where it is " << puzzle[i][j] << endl;
+        }
+    }
+
+    cout << sum;
+
     file.close();
     return 0;
+}
+
+void neighbors(int letter, int row, int col){
+    if(letter == COMPLETION){
+        sum++;
+        return;
+    }
+    if((row - 1) >= 0) {
+        if((col - 1) >= 0){ //(row-1, col-1)
+            if(puzzle[row-1][col-1] == letter + 1) neighbors(letter+1, row-1, col-1);
+        }
+        if((col + 1) < colMax){//(row-1, col+1)
+            if(puzzle[row-1][col+1] == letter + 1) neighbors(letter+1, row-1, col+1);
+        }
+        if(puzzle[row-1][col] == letter + 1) neighbors(letter+1, row-1, col);//(row-1, col)
+    }    
+    if((row + 1) < rowMax) {
+        if((col - 1) >= 0){//(row+1, col-1)
+            if(puzzle[row+1][col-1] == letter + 1) neighbors(letter+1, row+1, col-1);
+        }
+        if((col + 1) < colMax){//(row+1, col+1)
+            if(puzzle[row+1][col+1] == letter + 1) neighbors(letter+1, row+1, col+1);
+        }
+        if(puzzle[row+1][col] == letter + 1) neighbors(letter+1, row+1, col);//(row+1, col)
+    }
+    if((col - 1) >= 0){//(row, col-1)
+        if(puzzle[row][col-1] == letter + 1) neighbors(letter+1, row, col-1);
+    }
+    if((col + 1) < colMax){//(row, col+1)
+        if(puzzle[row][col+1] == letter + 1) neighbors(letter+1, row, col+1);
+    }
 }
