877ed6db1ab73f621dcda6351238456480825c7a
sophiacarlone <sophiamcar@gmail.com>
2025-01-05 15:14:42 -0500

    day4 done

--- day4.cpp
+++ day4.cpp
@@ -24,12 +24,13 @@ using namespace std;
 //Globals
 int rowMax;
 int colMax;
-int sum;
+int sum1; //part 1 answer
 vector<vector<int>> puzzle; //would like to avoid the heap if possible, but idk
 const map<char, int> m = {{'X', 0}, {'M', 1}, {'A', 2}, {'S', 3}};
     
 //Functions
 void neighbors(int letter, int direction, int row, int col);
+bool XFormat(int row, int col);
 
 int main()
 {
@@ -61,17 +62,27 @@ int main()
         }
     }
 
-    cout << sum << endl;
+    //Part 2
+    int sum2 = 0;
+    for(int i = 0; i < rowMax; i++){
+        for (int j = 0; j < colMax; j++){
+            if(puzzle[i][j] == m.at('A')){ //2 = A 
+                if (XFormat(i, j)) sum2++;
+            }
+        }
+    }
+
+    cout << sum1 << endl;
+    cout << sum2 << endl;
 
     file.close();
     return 0;
 }
 
+//part 1
 void neighbors(int letter, int direction, int row, int col){
-    cout << "letter is: " << letter << " at " << row << ", "<< col << endl;
     if(letter == COMPLETION){
-        cout << "hit" <<endl;
-        sum++;
+        sum1++;
         return;
     }
     if((row - 1) >= 0) {
@@ -101,4 +112,27 @@ void neighbors(int letter, int direction, int row, int col){
     if((direction == ALL || direction == RIGHT)&&((col + 1) < colMax)){//(row, col+1)
         if(puzzle[row][col+1] == letter + 1) neighbors(letter+1, RIGHT, row, col+1);
     }
+}
+
+//Part 2
+bool XFormat(int row, int col){
+    if(row == 0 || col == 0 || row == rowMax-1 || col == colMax-1) return false;
+    int topleft = puzzle[row-1][col-1];
+    int topright = puzzle[row-1][col+1];
+    int bottomleft = puzzle[row+1][col-1];
+    int bottomright = puzzle[row+1][col+1];
+
+    if(topleft == m.at('X') || topleft == m.at('A')) return false;
+    if(topright == m.at('X') || topright == m.at('A')) return false;
+    if(bottomleft == m.at('X') || bottomleft == m.at('A')) return false;
+    if(bottomright == m.at('X') || bottomright == m.at('A')) return false;
+
+    if (topleft == m.at('M') && bottomright != m.at('S'))return false;
+    else if (topleft == m.at('S') && bottomright != m.at('M'))return false;
+    else if (topleft = m.at('X')) return false;
+    if (topright == m.at('M') && bottomleft != m.at('S'))return false;
+    else if (topright == m.at('S') && bottomleft != m.at('M'))return false;
+    else if (topright == m.at('X')) return false;
+    
+    return true;
 }
